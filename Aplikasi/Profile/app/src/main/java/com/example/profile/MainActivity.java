2a67a33d530b4134972ddc020457f359c17fe824
abdullahnz <abdullahnz@github.com>
2020-08-17 12:53:21 +0700

    commiting project

---
+++ Aplikasi/Profile/app/src/main/java/com/example/profile/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.net.Uri;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.button);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://abdullahnz.github.io"));
+                startActivity(intent);
+            }
+        });
+    }
+}
+
+
